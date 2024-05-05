862e7120003e5d46cd66793e06df5d33e872f34f
Filipa Andrade <fandrade@laterpay.net>
2024-05-05 20:45:19 +0200

    Fix typo

--- logic.py
+++ logic.py
@@ -77,8 +77,8 @@ def read_joke(joke: str) -> str:
 def draw_joke(joke: str, joke_id: str = None) -> str:
     if joke_id:  # Get the explanation from the db
         joke = get_joke_by_id(joke_id=joke_id)
-        if joke.image_url:
-            return joke.image_url
+        if joke.image:
+            return joke.image
 
     # There is no joke_id or no explanation in the db, we get it from the openai api
     response = openai_client.images.generate(
